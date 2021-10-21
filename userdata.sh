38a4c7afa8c4a4c6bcce831d5f54a90f3c2307f9
manoj <manoj4mgl@gmail.com>
2021-10-21 15:29:23 +0530

    Changed docker installtion in userdata

--- userdata.sh
+++ userdata.sh
@@ -1,6 +1,7 @@
 # !/bin/bash
 sudo apt update -y
-sudo apt install -y docker.io
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
 sudo usermod -aG docker ubuntu
 sudo systemctl daemon-reload
 sudo docker run -d --name address -p 8080:80 manoj523/addressbook-app
